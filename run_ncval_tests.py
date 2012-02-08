3b9b362b8bcf0a15dc83406a8983020946233a91
Egor Pasko <pasko@google.com>
2012-02-08 17:08:14 +0400

    Cut inputs in pieces

--- run_ncval_tests.py
+++ run_ncval_tests.py
@@ -30,46 +30,98 @@ def PrintError(msg):
   print >> sys.stderr, 'error: %s' % msg
 
 
+def CheckAsm(asm, asmfile, gas, decoder, validator):
+  WriteFile(asmfile, asm)
+  basename = asmfile[:-2]
+  objfile = basename + '.o'
+  gas_command = [gas, asmfile, '-o', objfile]
+  retcode = subprocess.check_call(gas_command)
+  decoder_process = subprocess.Popen([decoder, objfile], stdout=subprocess.PIPE)
+  (decode_out, decode_err) = decoder_process.communicate()
+  WriteFile(basename + '.decode.out', decode_out)
+  validator_process = subprocess.Popen([validator, objfile],
+                                       stdout=subprocess.PIPE)
+  (val_out, val_err) = validator_process.communicate()
+  # WriteFile(asmfile[:-2] + '.val.out', val_out)
+  print val_out
+  return []
+
+
+def FillOneBundle(start_pos, total_bytes, insts):
+  new_pos = start_pos
+  bytes_written = 0
+  seen_bytes = 0
+  asm = '.text\n'
+  for inst in insts:
+    sep = '.byte 0x'
+    new_pos = start_pos + bytes_written
+    for byte in inst:
+      if seen_bytes >= start_pos:
+        asm += sep + byte
+        bytes_written += 1
+        if bytes_written == 32:
+          break
+        sep = ', 0x'
+      seen_bytes += 1
+    if seen_bytes >= start_pos:
+      asm += '\n'
+    if bytes_written == 32:
+      break
+  if bytes_written == 0:
+    return (None, None)
+  for i in xrange((32 - (bytes_written % 32)) % 32):
+    asm += 'nop\n'
+  if start_pos + bytes_written == total_bytes:
+    return (asm, total_bytes)
+  return (asm, new_pos)
+
+
+def CompareOffsets(off_list, hexfile):
+  return True
+
+
 def RunTest(tmp, gas, decoder, validator, test):
   hexfile = 'testdata/64/%s.hex' % test
   if not os.path.exists(hexfile):
     PrintError('%s: no such file' % hexfile)
     return False
-  asm = '.text\n'
-  bytes_written = 0
+
+  # Initialize the list of byte sequences representing instructions.
+  hex_instructions = []
+  total_bytes = 0
   for line in open(hexfile, 'r').readlines():
     if line.startswith('#'):
-      asm += line
       continue
-    sep = '.byte 0x'
+    one_inst = []
     for word in line.rstrip().split(' '):
       assert(re.match(r'[0-9a-zA-Z][0-9a-zA-Z]', word))
-      bytes_written += 1
-      asm += sep + word
-      sep = ', 0x'
-    asm = asm + '\n'
-  for i in xrange((32 - (bytes_written % 32)) % 32):
-    asm += 'nop\n'
-  asmfile = os.path.basename(hexfile[:-4]) + '.s'
-  asmfile = os.path.join(tmp, asmfile)
-  WriteFile(asmfile, asm)
-  objfile = asmfile[:-2] + '.o'
-  gas_command = [gas, asmfile, '-o', objfile]
-  retcode = subprocess.call(gas_command)
-  if retcode != 0:
-    PrintError('error while executing command: %s' % ' '.join(gas_command))
+      one_inst.append(word)
+      total_bytes += 1
+    if len(one_inst) != 0:
+      hex_instructions.append(one_inst)
+
+  # Cut the input instructions in bundles and run a test for each bundle.
+  start_pos = 0
+  runs = 0
+  top_errors = []
+  while True:
+    (asm, new_pos) = FillOneBundle(start_pos, total_bytes, hex_instructions)
+    if not asm:
+      break
+    assert(start_pos < new_pos)
+    start_pos = new_pos
+    asmfile = os.path.basename(hexfile[:-4]) + ('_part%d.s' % runs)
+    asmfile = os.path.join(tmp, asmfile)
+    err_offsets = CheckAsm(asm, asmfile, gas, decoder, validator)
+    runs += 1
+
+    # Collect offsets where validation errors occurred.
+    for off in err_offsets:
+      top_errors.append(start_pos + off)
+
+  # Compare the collected offsets with the golden file.
+  if not CompareOffsets(top_errors, hexfile):
     return False
-  decoder_process = subprocess.Popen([decoder, objfile], stdout=subprocess.PIPE)
-  (decode_out, decode_err) = decoder_process.communicate()
-  # TODO: compare decoder with a golden file or objdump again ..
-  print decode_out
-  validator_process = subprocess.Popen([validator, objfile],
-                                       stdout=subprocess.PIPE)
-  (val_out, val_err) = validator_process.communicate()
-  print 'stdout:'
-  print val_out
-  print 'stderr:'
-  print val_err
   return True
 
 
